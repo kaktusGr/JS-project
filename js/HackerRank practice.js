97907fad3f66177fb8d063e8910afabaf0b0d20d
Ekaterina Grishina <grischina.kotya@yandex.ru>
2023-04-04 22:36:08 +0700

    Add function breakingRecords

--- js/HackerRank practice.js
+++ js/HackerRank practice.js
@@ -33,3 +33,26 @@ function timeConversion(time) {
 		return time = time.slice(0, -2);
 	}
 }
+
+function breakingRecords(scores) {
+	let countMin = 0;
+	let countMax = 0;
+	let min = scores[0];
+	let max = scores[0];
+	for (let i = 0; i < scores.length; i++) {
+		if (scores[i] > max) {
+			max = scores[i];
+			countMax++;
+		} else if (scores[i] == max) {
+			max = scores[i];
+		} else if (scores[i] < max && scores[i] < min) {
+			min = scores[i];
+			countMin++;
+		} else if (scores[i] == min) {
+			min = scores[i];
+		}
+	}
+	return [countMax, countMin];
+}
+
+console.log(breakingRecords([12, 24, 10, 25, 6, 29, 20]));
