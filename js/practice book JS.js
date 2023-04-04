2f35e2fe81ec143b2935956e1b97372f90b76243
kaktusGr <124692458+kaktusGr@users.noreply.github.com>
2023-04-05 01:24:52 +0700

    Add function simple numbers

--- js/practice book JS.js
+++ js/practice book JS.js
@@ -1,39 +1,43 @@
 "use strict"
-/*
-//Use prompt and alert
 
-let nameJS = prompt('Какое официальное название JavaScript?', '');
-if (nameJS = ECMAScript) {
-	alert("Верно!");
-} else {
-	alert("Не знаете? ECMAScript!");
+function promptNameJS() {
+	let nameJS = prompt('Какое официальное название JavaScript?', '');
+	if (nameJS === 'ECMAScript') {
+		alert("Верно!");
+	} else {
+		alert("Не знаете? ECMAScript!");
+	}
 }
 
-let num = prompt('Введите число', '');
-if (num > 0) {
-	alert(1);
-} else if (num < 0) {
-	alert (-1);
-} else {
-	alert (0);
+function promptNumber() {
+	let num = prompt('Введите число', '');
+	if (num > 0) {
+		alert(1);
+	} else if (num < 0) {
+		alert(-1);
+	} else {
+		alert(0);
+	}
 }
 
-let userName = prompt('Кто там?', '');
-if (userName === 'Админ') {
-	let psw = prompt('Пароль?', '');
+function promptUserName() {
+	let userName = prompt('Кто там?', '');
+	if (userName === 'Админ') {
+		let psw = prompt('Пароль?', '');
 
-	if (psw === 'Я Главный') {
-		alert('Здравствуйте!');
-	} else if (psw === '' || psw === null) {
+		if (psw === 'Я Главный') {
+			alert('Здравствуйте!');
+		} else if (psw === '' || psw === null) {
+			alert('Отменено');
+		} else {
+			alert('Неверный пароль');
+		}
+
+	} else if (userName === '' || userName === null) {
 		alert('Отменено');
 	} else {
-		alert('Неверный пароль');
+		alert('Я вас не знаю');
 	}
-
-} else if (userName === '' || userName === null) {
-	alert('Отменено');
-} else {
-	alert('Я вас не знаю');
 }
 
 function myAge(birthday) {
@@ -58,4 +62,13 @@ let birthday = prompt('Write your birthday (DD.MM.YYYY)', '');
 if (birthday !== '') {
 	alert(myAge(birthday));
 }
-*/
+
+function simpleNum(num) {
+	stepNum:
+	for (let i = 2; i <= num; i++) {
+		for (let j = 2; j < i; j++) {
+			if (i % j == 0) continue stepNum;
+		}
+		console.log(i);
+	}
+}
