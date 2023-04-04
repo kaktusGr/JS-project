0f4d127fe3187ccdc8393ae6cbbe1d2f3dfbf9cd
Ekaterina Grishina <grischina.kotya@yandex.ru>
2023-04-05 01:08:30 +0700

    Refactoring

--- js/practice book JS.js
+++ js/practice book JS.js
@@ -1,72 +1,77 @@
 "use strict"
-/*
+
 //Use prompt and alert
-
-let nameJS = prompt('Какое официальное название JavaScript?', '');
-if (nameJS = ECMAScript) {
-	alert("Верно!");
-} else {
-	alert("Не знаете? ECMAScript!");
+function promptName() {
+	let nameJS = prompt('Какое официальное название JavaScript?', '');
+	if (nameJS == 'ECMAScript') {
+		alert("Верно!");
+	} else {
+		alert("Не знаете? ECMAScript!");
+	}
 }
 
-let num = prompt('Введите число', '');
-if (num > 0) {
-	alert(1);
-} else if (num < 0) {
-	alert (-1);
-} else {
-	alert (0);
+function promptNumber() {
+	let num = prompt('Введите число', '');
+	if (num > 0) {
+		alert(1);
+	} else if (num < 0) {
+		alert(-1);
+	} else {
+		alert(0);
+	}
 }
 
-let userName = prompt('Кто там?', '');
-if (userName === 'Админ') {
-	let psw = prompt('Пароль?', '');
+function promptUserName() {
+	let userName = prompt('Кто там?', '');
+	if (userName === 'Админ') {
+		let psw = prompt('Пароль?', '');
 
-	if (psw === 'Я Главный') {
-		alert('Здравствуйте!');
-	} else if (psw === '' || psw === null) {
+		if (psw === 'Я Главный') {
+			alert('Здравствуйте!');
+		} else if (psw === '' || psw === null) {
+			alert('Отменено');
+		} else {
+			alert('Неверный пароль');
+		}
+
+	} else if (userName === '' || userName === null) {
 		alert('Отменено');
 	} else {
-		alert('Неверный пароль');
-	}
-
-} else if (userName === '' || userName === null) {
-	alert('Отменено');
-} else {
-	alert('Я вас не знаю');
-}
-
-function myAge(birthday) {
-	let myYear = birthday.slice(-4);
-	let nowYear = date.getFullYear();
-	let myDay = birthday.slice(0, 2);
-	let nowDay = date.getDate();
-	let myMonth = birthday.slice(3, 5);
-	let nowMonth = date.getMonth() + 1;
-
-	if ((myMonth < nowMonth) || (myMonth == nowMonth && myDay < nowDay)) {
-		return `Your age is ${nowYear - myYear} years`;
-	} else if ((myMonth > nowMonth) || (myMonth == nowMonth && myDay > nowDay)) {
-		return `Your age is ${nowYear - myYear - 1} years`;
-	} else {
-		return 'Happy birthday!';
+		alert('Я вас не знаю');
 	}
 }
 
-let date = new Date();
-let birthday = prompt('Write your birthday (DD.MM.YYYY)', '');
-if (birthday !== '') {
-	alert(myAge(birthday));
-}
+function promptBirthday() {
+	function myAge(birthday) {
+		let myYear = birthday.slice(-4);
+		let nowYear = date.getFullYear();
+		let myDay = birthday.slice(0, 2);
+		let nowDay = date.getDate();
+		let myMonth = birthday.slice(3, 5);
+		let nowMonth = date.getMonth() + 1;
 
-//Выводим все простые числа в диапазоне num
-let num = 30;
-
-stepNum:
-for (let i = 2; i <= num; i++) {
-	for (let j = 2; j < i; j++) {
-		if (i % j == 0) continue stepNum;
+		if ((myMonth < nowMonth) || (myMonth == nowMonth && myDay < nowDay)) {
+			return `Your age is ${nowYear - myYear} years`;
+		} else if ((myMonth > nowMonth) || (myMonth == nowMonth && myDay > nowDay)) {
+			return `Your age is ${nowYear - myYear - 1} years`;
+		} else {
+			return 'Happy birthday!';
+		}
 	}
-	console.log(i);
+
+	let date = new Date();
+	let birthday = prompt('Write your birthday (DD.MM.YYYY)', '');
+	if (birthday !== '') {
+		alert(myAge(birthday));
+	}
 }
-*/
+
+function simpleNum(num) {
+	stepNum:
+	for (let i = 2; i <= num; i++) {
+		for (let j = 2; j < i; j++) {
+			if (i % j == 0) continue stepNum;
+		}
+		console.log(i);
+	}
+}
