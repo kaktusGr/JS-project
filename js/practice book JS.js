b27af90ed3e4a7608ca347eb47ec3cf007d0b089
Ekaterina Grishina <grischina.kotya@yandex.ru>
2023-04-12 14:53:25 +0300

    Add function filterRangeInPlace

--- js/practice book JS.js
+++ js/practice book JS.js
@@ -140,3 +140,14 @@ function camelize(str) {
 
 	return upperCase.join('');
 }
+
+function filterRangeInPlace(arr, a, b) {
+
+	for (let i = 0; i < arr.length; i++) {
+		let val = arr[i];
+		if (val < a || val > b) {
+			arr.splice(i, 1);
+			i--;
+		}
+	}
+}
