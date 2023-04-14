c76c7518d92bf78a02a7d96aa9c612dee439350d
Ekaterina Grishina <grischina.kotya@yandex.ru>
2023-04-14 14:00:31 +0300

    Add sorting methods

--- js/practice book JS.js
+++ js/practice book JS.js
@@ -135,14 +135,11 @@ function getMaxSubSumShort(arr) {
 
 function camelize(str) {
 	let strToArray = str.split('-');
-
 	let upperCase = strToArray.map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1));
-
 	return upperCase.join('');
 }
 
 function filterRangeInPlace(arr, a, b) {
-
 	for (let i = 0; i < arr.length; i++) {
 		let val = arr[i];
 		if (val < a || val > b) {
@@ -151,12 +148,6 @@ function filterRangeInPlace(arr, a, b) {
 		}
 	}
 }
-
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
-
-let users = [vasya, petya, masha];
 
 let usersMapped = users.map(item => {
 	let newArr = {};
@@ -169,3 +160,70 @@ let usersMapped = users.map(item => {
 // 	fullName: `${item.name} ${item.surname}`,
 // 	id: item.id
 //  }));
+
+function sortByAge(arr) {
+	arr.sort((user1, user2) => {
+		if (user1.age > user2.age) {
+			return 1;
+		}
+		if (user1.age < user2.age) {
+			return -1;
+		}
+		return 0;
+	})
+}
+
+/*
+// Example how to use sorting algorithms with different arrays
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arrayOfUsers = [vasya, petya, masha];
+let arrayOfNumbers = [10, 5, 3, 15];
+let arrayOfStrings = ["a", "zzzzz", "vv", "bbbbbbb"];
+
+function bubbleSortReverseToBegin(array, compareFunction) {
+	const copyArray = array.slice();
+	for (let k = copyArray.length; k > 0; k--) {
+		for (let i = copyArray.length - 1; i > 0; i--) {
+			if (compareFunction(copyArray[i], copyArray[i - 1]) > 0) {
+				let buffer = copyArray[i];
+				copyArray[i] = copyArray[i - 1];
+				copyArray[i - 1] = buffer;
+			}
+		}
+	}
+	return copyArray;
+}
+
+function compareUsersByAge(user1, user2) {
+	if (user1.age > user2.age) {
+		return 1;
+	}
+	if (user1.age < user2.age) {
+		return -1;
+	}
+	return 0;
+}
+
+function compareNumbers(number1, number2) {
+	if (number1 > number2) {
+		return 1;
+	}
+	if (number1 < number2) {
+		return -1;
+	}
+	return 0;
+}
+
+function compareStringsByLength(string1, string2) {
+	if (string1.length > string2.length) {
+		return 1;
+	}
+	if (string1.length < string2.length) {
+		return -1;
+	}
+	return 0;
+}
+*/
