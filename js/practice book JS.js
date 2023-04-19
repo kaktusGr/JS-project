482f772a1a9918f8daa876cf681233653c6d5099
Ekaterina Grishina <grischina.kotya@yandex.ru>
2023-04-19 17:15:50 +0300

    Add function groupById

--- js/practice book JS.js
+++ js/practice book JS.js
@@ -269,3 +269,16 @@ let calc = new Calculator;
 // calc.addMethod("*", (a, b) => a * b);
 // console.log(calc.calculate("2 * 3"));
 // console.log(calc.calculate("3 + 7"));
+
+// let users = [
+// 	{id: 'john', name: "John Smith", age: 20},
+// 	{id: 'ann', name: "Ann Smith", age: 24},
+// 	{id: 'pete', name: "Pete Peterson", age: 31},
+//  ];
+ 
+ function groupById(array) {
+	return array.reduce((obj, value) => {
+		 obj[value.id] = value;
+		 return obj;
+	}, {})
+ }
