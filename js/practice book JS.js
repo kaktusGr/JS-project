5039118698715c7486b2036af165331678773755
Ekaterina Grishina <grischina.kotya@yandex.ru>
2023-05-04 15:08:57 +0300

    Cleaning functions with arrays

--- js/practice book JS.js
+++ js/practice book JS.js
@@ -126,17 +126,11 @@ function getMaxSubSumShort(arr) {
 	return maxSum;
 }
 
-/*function camelize(str) {
+function camelize(str) {
 	return str
 		.split('-')
 		.map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
 		.join('');
-}*/
-
-function camelize(str) {
-	let strToArray = str.split('-');
-	let upperCase = strToArray.map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1));
-	return upperCase.join('');
 }
 
 function filterRangeInPlace(arr, a, b) {
@@ -156,11 +150,6 @@ let usersMapped = users.map(item => {
 	return newArr;
 });
 
-// let usersMapped = users.map(item => ({
-// 	fullName: `${item.name} ${item.surname}`,
-// 	id: item.id
-//  }));
-
 function sortByAge(arr) {
 	arr.sort((user1, user2) => {
 		if (user1.age > user2.age) {
@@ -172,61 +161,6 @@ function sortByAge(arr) {
 		return 0;
 	})
 }
-
-/*
-// Example how to use sorting algorithms with different arrays
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
-
-let arrayOfUsers = [vasya, petya, masha];
-let arrayOfNumbers = [10, 5, 3, 15];
-let arrayOfStrings = ["a", "zzzzz", "vv", "bbbbbbb"];
-
-function bubbleSortReverseToBegin(array, compareFunction) {
-	const copyArray = array.slice();
-	for (let k = copyArray.length; k > 0; k--) {
-		for (let i = copyArray.length - 1; i > 0; i--) {
-			if (compareFunction(copyArray[i], copyArray[i - 1]) > 0) {
-				let buffer = copyArray[i];
-				copyArray[i] = copyArray[i - 1];
-				copyArray[i - 1] = buffer;
-			}
-		}
-	}
-	return copyArray;
-}
-
-function compareUsersByAge(user1, user2) {
-	if (user1.age > user2.age) {
-		return 1;
-	}
-	if (user1.age < user2.age) {
-		return -1;
-	}
-	return 0;
-}
-
-function compareNumbers(number1, number2) {
-	if (number1 > number2) {
-		return 1;
-	}
-	if (number1 < number2) {
-		return -1;
-	}
-	return 0;
-}
-
-function compareStringsByLength(string1, string2) {
-	if (string1.length > string2.length) {
-		return 1;
-	}
-	if (string1.length < string2.length) {
-		return -1;
-	}
-	return 0;
-}
-*/
 
 function shuffle(array) {
 	for (let i = array.length - 1; i > 0; i--) {
@@ -263,18 +197,7 @@ function Calculator() {
 		this[key] = func;
 	}
 }
-
 let calc = new Calculator;
-
-// calc.addMethod("*", (a, b) => a * b);
-// console.log(calc.calculate("2 * 3"));
-// console.log(calc.calculate("3 + 7"));
-
-// let users = [
-// 	{id: 'john', name: "John Smith", age: 20},
-// 	{id: 'ann', name: "Ann Smith", age: 24},
-// 	{id: 'pete', name: "Pete Peterson", age: 31},
-//  ];
 
 function groupById(array) {
 	return array.reduce((obj, value) => {
